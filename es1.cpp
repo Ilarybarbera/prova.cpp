2f398d6d13fd4368db79a89ebb6d1e9cec08e81e
Ilarybarbera <barberailary@gmail.com>
2025-01-10 10:14:53 +0000

    es1

--- es1.cpp
+++ es1.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
 using namespace std;
 int main()
-{ int m1[2][2];
-  int m2[2][2];
+{ int m[2][2];
   int r;
   int c;
-  int max=0;
   
-  for(int r=0; r<2;r++) {
-      
+  for(int r=0; r<2;r++) {   
 for(int c=0;c<2;c++) {
 std::cout << "inserisci i numeri" << std::endl;
-   std::cin >> m1[r][c]; 
-   if(m1[r][c]>max){
-       max=m1[r][c];
+   std::cin >> m[r][c]; 
+   if(m[r][c]==3){
+    std::cout << "il numero 3 è presente" << std::endl;   
    }
-}
-}
-for(int r=0; r<2;r++) {
-      
-for(int c=0;c<2;c++) {
-m2[r][c]=max*m1[r][c];
-}
-}
-for(int r=0; r<2;r++) {
-      
-for(int c=0;c<2;c++) {
-std::cout << m2[r][c] << ""; 
+   else{
+    std::cout << "il numero 3 non è presente" << std::endl; 
+   }
 }
 }
 }
